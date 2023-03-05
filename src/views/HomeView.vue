f69e6d3e6658d5e3677d7061425a7f5f11086309
Daniel Choi <schoiak@connect.ust.hk>
2023-03-05 22:10:52 +0900

    Add - Home view added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,189 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <section class="bg-white dark:bg-gray-900">
+      <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
+          <p class="mb-4">
+            We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big
+            enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the
+            scope you want at the pace you need.
+          </p>
+          <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+        </div>
+        <div class="grid grid-cols-2 gap-4 mt-8">
+          <img class="w-full rounded-lg object-scale-down" src="@/assets/demo.png" alt="office content 1" />
+          <img class="mt-4 w-full lg:mt-10 rounded-lg object-scale-down" src="@/assets/demo2.png" alt="office content 2" />
+        </div>
+      </div>
+    </section>
+
+    <section class="pt-16 pb-32">
+      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-6 xl:gap-12 2xl:gap-24">
+        <div class="max-w-xl px-4 mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 space-y-6 lg:col-start-2">
+          <div>
+            <h2 class="text-4xl font-extrabold tracking-tight text-gray-900">Personalized approach<span class="text-primary">.</span></h2>
+            <p class="mt-4 text-lg leading-relaxed text-gray-500 sm:text-xl">
+              We offer a tailored and personalized approach to learning English. Our experienced teachers help you get the most out of your
+              learning experience.
+            </p>
+          </div>
+          <div><!----></div>
+          <!---->
+        </div>
+        <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+          <div class="lg:px-0 lg:m-0 lg:relative lg:h-full lg:flex lg:items-center pr-4 -sm:ml-48 sm:pr-6 md:-ml-16">
+            <img
+              class="w-full lg:absolute lg:w-auto lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl rounded lg:right-0"
+              src="https://images.unsplash.com/photo-1509062522246-3755977927d7?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=MnwzMzczODV8MHwxfHNlYXJjaHw3fHxlZHVjYXRpb258ZW58MHwwfHx8MTY3NzY4MjY0Mw&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1080"
+              alt="Personalized approach"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="py-12 overflow-hidden bg-primary bg-opacity-80 md:py-20 bg-slate-800"
+      sectionname="testimonials"
+      id="k1nwdj9hxi"
+      sectioncomponent="Testimonials"
+      sortorder="2"
+      site-name="Private English Learning Center"
+      site-id="igy3Tk0Qo5fsN9PVaT5C"
+      site-logo-size="md"
+    >
+      <div class="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <svg
+          class="absolute transform top-full right-full translate-x-1/3 -translate-y-1/4 lg:translate-x-1/2 xl:-translate-y-1/2 rotate-3"
+          width="404"
+          height="404"
+          fill="none"
+          viewBox="0 0 404 404"
+          role="img"
+          aria-labelledby="svg-squares"
+        >
+          <title id="svg-squares">squares</title>
+          <defs>
+            <pattern id="ad119f34-7694-4c31-947f-5c9d249b21f3" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-primary" fill="currentColor"></rect>
+            </pattern>
+          </defs>
+          <rect width="404" height="404" fill="url(#ad119f34-7694-4c31-947f-5c9d249b21f3)"></rect>
+        </svg>
+        <div class="relative">
+          <blockquote>
+            <div class="max-w-3xl mx-auto text-xl font-bold leading-7 text-center text-white md:leading-10 md:text-3xl text-shadow-sm">
+              <p>"The private learning center was a great way to take the stress out of learning."</p>
+            </div>
+            <footer class="mt-8">
+              <div class="md:flex md:items-center md:justify-center">
+                <div class="md:flex-shrink-0">
+                  <img
+                    src="https://storage.googleapis.com/mixo-files/public/img/avatars/female-10.png"
+                    class="w-10 h-10 mx-auto border-2 border-slate-200 rounded-full shadow-sm object-cover"
+                    alt="Testimonial"
+                  />
+                </div>
+                <div class="mt-3 text-center md:mt-0 md:ml-3 md:flex md:items-center text-shadow-sm">
+                  <div class="text-lg font-medium text-white">Sally Belcastro</div>
+                  <!---->
+                </div>
+              </div>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+    </section>
+
+    <section
+      class="relative py-12 sm:py-20 md:py-24 lg:py-32"
+      sectionname="cta"
+      id="cab03fmp4a"
+      sectioncomponent="Cta"
+      sortorder="4"
+      site-name="Private English Learning Center"
+      site-logo-url="https://storage.googleapis.com/mixo-files/logos/privateEnglishLearningCenter-1677768430244.svg"
+      site-logo-size="md"
+      data-v-79ccdc36=""
+    >
+      <div aria-hidden="true" class="block" data-v-79ccdc36="">
+        <div class="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl mb-12" data-v-79ccdc36=""></div>
+        <svg class="absolute -ml-3 top-8 left-1/2" width="404" height="392" fill="none" viewBox="0 0 404 392" data-v-79ccdc36="">
+          <defs data-v-79ccdc36="">
+            <pattern
+              id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+              data-v-79ccdc36=""
+            >
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" data-v-79ccdc36=""></rect>
+            </pattern>
+          </defs>
+          <rect width="404" height="392" fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)" data-v-79ccdc36=""></rect>
+        </svg>
+      </div>
+      <div class="max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8" data-v-79ccdc36="">
+        <div
+          class="relative px-6 py-10 overflow-hidden bg-white border-primary border-2 shadow-xl rounded-2xl sm:px-12 sm:py-20"
+          data-v-79ccdc36=""
+        >
+          <div aria-hidden="true" class="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0" data-v-79ccdc36="">
+            <svg
+              class="absolute inset-0 w-full h-full"
+              preserveAspectRatio="xMidYMid slice"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 1463 360"
+              data-v-79ccdc36=""
+            >
+              <path
+                class="text-primary/5"
+                fill="currentColor"
+                d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+                data-v-79ccdc36=""
+              ></path>
+              <path
+                class="text-primary/10"
+                fill="currentColor"
+                d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+                data-v-79ccdc36=""
+              ></path>
+            </svg>
+          </div>
+          <div class="relative" data-v-79ccdc36="">
+            <div class="mt-6 sm:mt-12 sm:text-center" data-v-79ccdc36="">
+              <h2
+                class="text-3xl font-black tracking-tight text-gray-900 sm:text-4xl md:text-5xl md:leading-tight md:max-w-4xl md:mx-auto"
+                data-v-79ccdc36=""
+              >
+                Join our community of fans that love Private English Learning Center<span class="text-primary" data-v-79ccdc36="">.</span>
+              </h2>
+              <!---->
+            </div>
+            <div class="mt-6 sm:mt-12 sm:mx-auto sm:max-w-lg flex flex-col items-center" data-v-79ccdc36=""><!----></div>
+            <form
+              class="grid gap-2 grid-cols-1 sm:w-full sm:flex sm:items-center sm:flex-wrap mt-6 sm:mt-12 sm:mx-auto sm:max-w-lg"
+              client:load=""
+              data-v-79ccdc36=""
+            >
+              <label for="cta-email" class="sr-only">Email address</label
+              ><input
+                id="cta-email"
+                type="email"
+                class="block w-full px-5 py-3 text-base text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:border-primary focus:ring-primary focus-visible:ring-primary flex-1"
+                placeholder="Enter your email..."
+              />
+              <div class="bg-white"><button class="ui-button ui-button-base ui-button-primary">Join the waiting list</button></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
---
+++ src/views/index.ts
@@ -0,0 +1,19 @@
+import HomeView from './HomeView.vue'
+import AboutView from './AboutView.vue'
+import LocationView from './about/LocationView.vue'
+import StaffView from './about/StaffView.vue'
+import PhilosophyView from './about/PhilosophyView.vue'
+import AdmissionView from './AdmissionView.vue'
+import CommunityView from './CommunityView.vue'
+import CurriculumView from './CurriculumView.vue'
+
+export { 
+    HomeView, 
+    AboutView, 
+    LocationView,
+    StaffView,
+    PhilosophyView,
+    AdmissionView,
+    CommunityView,
+    CurriculumView,
+}
